27dcd00e4ca65ef790a3e38657dd4c6223a02dc6
Ryan Clark <rclark.sf@gmail.com>
2011-07-23 23:13:23 -0700

    Ajax applied, voting enabled

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -15,4 +15,7 @@ class StoriesController < ApplicationController
 			render :action => 'new'
 		end
 	end
+	def show 
+		@story = Story.find(params[:id])
+	end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,12 @@
 class Story < ActiveRecord::Base
 	validates_presence_of :name, :link
+	has_many :votes
+	def to_param
+		"#{id}-#{name.gsub(/\W/, '-').downcase}"
+	end
+	has_many :votes do
+		def latest
+			find :all, :order => 'id DESC', :limit => 3
+		end
+	end
 end
